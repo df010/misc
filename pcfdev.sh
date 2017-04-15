9034d27f92b7ee0a39bc6c33a1a6d7a9bf6f8dee
root <wudongfei@gmail.com>
2017-04-15 22:49:01 +0800

    update

---
+++ pcfdev.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+##run this command in pcfdev vm. (cf dev ssh)
+sudo su -
+cd /
+swapoff -a
+dd if=/dev/zero of=/swapfile bs=1M count=16384
+mkswap /swapfile
+swapon /swapfile
+sed -e "s/memoryMB=[0-9]*/memoryMB=65536/" -e "s/diskMB=[0-9]*/diskMB=65536/" /var/vcap/jobs/rep/bin/rep_as_vcap
+/var/vcap/bosh/bin/monit restart rep
