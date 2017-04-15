6f6469432878e20639b0a1bf82d04f1cccfc1ac1
root <wudongfei@gmail.com>
2017-04-15 22:56:24 +0800

    update

--- pcfdev.sh
+++ pcfdev.sh
@@ -6,5 +6,5 @@ swapoff -a
 dd if=/dev/zero of=/swapfile bs=1M count=16384
 mkswap /swapfile
 swapon /swapfile
-sed -e "s/memoryMB=[0-9]*/memoryMB=65536/" -e "s/diskMB=[0-9]*/diskMB=65536/" /var/vcap/jobs/rep/bin/rep_as_vcap
+sed -i -e "s/memoryMB=[0-9]*/memoryMB=65536/" -e "s/diskMB=[0-9]*/diskMB=65536/" /var/vcap/jobs/rep/bin/rep_as_vcap
 /var/vcap/bosh/bin/monit restart rep
